2013949dd95850a474bf1f0bf2eb747fab36a290
rotpingo <viorel.moschoi@gmail.com>
2025-03-13 18:00:03 +0100

    added some functionality to submit searchCity

--- frontend/src/app/components/footbar/footbar.component.ts
+++ frontend/src/app/components/footbar/footbar.component.ts
@@ -1,20 +1,24 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { WeatherService } from '../../services/weather.service';
 
 @Component({
   selector: 'app-footbar',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './footbar.component.html',
   styleUrl: './footbar.component.css'
 })
 export class FootbarComponent {
 
-  @ViewChild('modal', {static:true}) modal!: ElementRef;
-  @ViewChild('cover', {static:true}) cover!: ElementRef;
-  @ViewChild('search', {static:true}) search!: ElementRef;
+  @ViewChild('modal', { static: true }) modal!: ElementRef;
+  @ViewChild('cover', { static: true }) cover!: ElementRef;
+  @ViewChild('search', { static: true }) search!: ElementRef;
 
-  isModalOpen: boolean = false;
+  city = new FormControl('', [Validators.required, Validators.minLength(1)]);
 
-  closeModal(){
+  constructor(private service: WeatherService){}
+
+  closeModal() {
     const coverElement = this.cover.nativeElement;
     coverElement.style.display = "none";
     const modalElement = this.modal.nativeElement;
@@ -23,18 +27,30 @@ export class FootbarComponent {
     searchElement.style.display = "none";
   }
 
-  openModal(){
+  openModal() {
     const coverElement = this.cover.nativeElement;
     coverElement.style.display = "flex";
     const modalElement = this.modal.nativeElement;
     modalElement.style.display = "flex";
   }
 
-  openSearch(){
+  openSearch() {
     const searchElement = this.search.nativeElement;
     searchElement.style.display = "flex";
     const modalElement = this.modal.nativeElement;
     modalElement.style.display = "none";
+  }
+
+  onSearchCity() {
+    if (this.city.valid && this.city.value != '') {
+      this.service.getWeather(this.city.value!).subscribe({
+   
+      })
+      console.log("value: ", this.city.value);
+    } else {
+      console.log('Invalid input');
+    }
+
   }
 
 }
