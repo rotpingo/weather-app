44a9c5d823cb8a48328e57501a5a430b7faceba2
rotpingo <viorel.moschoi@gmail.com>
2025-03-13 16:29:23 +0100

    added hamburger menu icon, modal + cover

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WeatherComponent } from './components/weather/weather.component';
+import { FootbarComponent } from './components/footbar/footbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, WeatherComponent],
+  imports: [RouterOutlet, WeatherComponent, FootbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
